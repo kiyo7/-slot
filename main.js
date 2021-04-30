6bac6d3a7320bd17e1b6128ae62184cda1e113c6
kiyo <kiyo324783@icloud.com>
2021-04-30 16:17:24 +0900

    3 commit

--- main.js
+++ main.js
@@ -1,25 +1,13 @@
 //初期化　定義
+let turn = 3;
+
 let nowTimer1;
 let nowTimer2;
 let nowTimer3;
-let nowTimer4;
-let nowTimer5;
-let nowTimer6;
-let nowTimer7;
-let nowTimer8;
-let nowTimer9;
 
-let count1 = 0;
-let count2 = 0;
-let count3 = 0;
-let count4 = 8;
-let count5 = 8;
-let count6 = 8;
-let count7 = 1;
-let count8 = 1;
-let count9 = 1;
-
-let turn = 3;
+let count1 = count2 = count3 = 0;
+let count4 = count5 = count6 = 9;
+let count7 = count8 = count9 = 1;
 
 const nowTime1   = document.getElementById('nowTime1'); // スロット1〜9
 const nowTime2   = document.getElementById('nowTime2');
@@ -51,87 +39,87 @@ setTime1.disabled = true;
 setTime2.disabled = true;
 setTime3.disabled = true;
 
-//処理
+//↓処理
 startTimer.addEventListener('click', function() { //スタート押されたら
     setTime1.disabled = false
     setTime2.disabled = false
     setTime3.disabled = false
-
     clearInterval(nowTimer1);
     clearInterval(nowTimer2);
     clearInterval(nowTimer3);
-    clearInterval(nowTimer4);
-    clearInterval(nowTimer5);
-    clearInterval(nowTimer6);
-    clearInterval(nowTimer7);
-    clearInterval(nowTimer8);
-    clearInterval(nowTimer9);
     cntUp();
     startTimer.disabled = true
     turn = 3;
 })
 
-const cntUp = function() { //スロット処理 * 9
-    nowTimer1 = setInterval(function() {
-        count1 += 1;
-        nowTime1.textContent = count1;
-        if (count1 === 9) count1 = 0;
+const cntUp = function() { //カウントアップ処理
+        nowTimer1 = setInterval(function() {
+        if(count1 === 0) {
+            count1 = 0;
+            nowTime4.textContent = 9;
+            nowTime1.textContent = count1;
+            nowTime7.textContent = count1+1;
+            count1 += 1;
+        } else if (count1 === 9) {
+            count1 = 9;
+            nowTime4.textContent = count1 -1;
+            nowTime1.textContent = count1;
+            nowTime7.textContent = 0;
+            count1 = 0
+        } else {
+            nowTime4.textContent = count1 -1;
+            nowTime1.textContent = count1;
+            nowTime7.textContent = count1 +1;
+            count1 += 1;
+        }
     }, 100);
 
     nowTimer2 = setInterval(function() {
-        count2 += 1;
-        nowTime2.textContent = count2;
-        if (count2 === 9) count2 = 0;
+        if(count2 === 0) {
+            count2 = 0;
+            nowTime5.textContent = 9;
+            nowTime2.textContent = count2;
+            nowTime8.textContent = count2+1;
+            count2 += 1;
+        } else if (count2 === 9) {
+            count2 = 9;
+            nowTime5.textContent = count2 -1;
+            nowTime2.textContent = count2;
+            nowTime8.textContent = 0;
+            count2 = 0
+        } else {
+            nowTime5.textContent = count2 -1;
+            nowTime2.textContent = count2;
+            nowTime8.textContent = count2 +1;
+            count2 += 1;
+        }
     }, 100);
 
     nowTimer3 = setInterval(function() {
-        count3 += 1;
-        nowTime3.textContent = count3;
-        if (count3 === 9) count3 = 0;
+        if(count3 === 0) {
+            count3 = 0;
+            nowTime6.textContent = 9;
+            nowTime3.textContent = count3;
+            nowTime9.textContent = count3 +1;
+            count3 += 1;
+        } else if (count3 === 9) {
+            count3 = 9;
+            nowTime6.textContent = count3 -1;
+            nowTime3.textContent = count3;
+            nowTime9.textContent = 0;
+            count3 = 0
+        } else {
+            nowTime6.textContent = count3 -1;
+            nowTime3.textContent = count3;
+            nowTime9.textContent = count3 +1;
+            count3 += 1;
+        }
     }, 100);
-
-    nowTimer4 = setInterval(function() {
-        count4 += 1;
-        nowTime4.textContent = count4;
-        if (count4 === 9) count4 = 0;
-    }, 100);
-
-    nowTimer5 = setInterval(function() {
-        count5 += 1;
-        nowTime5.textContent = count5;
-        if (count5 === 9) count5 = 0;
-    }, 100);
-
-    nowTimer6 = setInterval(function() {
-        count6 += 1;
-        nowTime6.textContent = count6;
-        if (count6 === 9) count6 = 0;
-    }, 100);
-
-    nowTimer7 = setInterval(function() {
-        count7 += 1;
-        nowTime7.textContent = count7;
-        if (count7 === 9) count7 = 0;
-    }, 100);
-
-    nowTimer8 = setInterval(function() {
-        count8 += 1;
-        nowTime8.textContent = count8;
-        if (count8 === 9) count8 = 0;
-    }, 100);
-
-    nowTimer9 = setInterval(function() {
-        count9 += 1;
-        nowTime9.textContent = count9;
-        if (count9 === 9) count9 = 0;
-    }, 100);
-};
+}
 
 setTime1.addEventListener('click', function(){//ストップ押されたら
     setTime1.disabled = true;
-    clearInterval(nowTimer1); 
-    clearInterval(nowTimer4);　//上下　±3
-    clearInterval(nowTimer7);
+    clearInterval(nowTimer1);
     turn -= 1;
     check();
 })
@@ -139,16 +127,12 @@ setTime1.addEventListener('click', function(){//ストップ押されたら
 setTime2.addEventListener('click', function(){//ストップ押されたら
     setTime2.disabled = true;
     clearInterval(nowTimer2);
-    clearInterval(nowTimer5);
-    clearInterval(nowTimer8);
     turn -= 1;
     check();
 })
 setTime3.addEventListener('click', function(){//ストップ押されたら
     setTime3.disabled = true;
     clearInterval(nowTimer3);
-    clearInterval(nowTimer6);
-    clearInterval(nowTimer9);
     turn -= 1;
     check();
 })
@@ -164,13 +148,13 @@ const check = () => {
                 alert('おめでとう');
         }
     else if (
-            nowTime4.textContent === nowTime2.textContent &&
+            nowTime4.textContent === nowTime2.textContent && //斜め
             nowTime4.textContent === nowTime9.textContent
             ) {
                 alert('おめでとう');
         }
     else if (
-            nowTime6.textContent === nowTime2.textContent &&
+            nowTime6.textContent === nowTime2.textContent && //斜め
             nowTime6.textContent === nowTime7.textContent
              ) {
                 alert('おめでとう');
@@ -183,4 +167,3 @@ const check = () => {
     setTime3.disabled   = false
     startTimer.disabled = false
 };
-
